6e1d1c907abf36ad6695e3330746022d08ecebe2
Thomas Day <git@tday.au>
2025-10-27 22:28:00 +1000

    Implement decoder. Fix bug where an activation layer was not initialised. Fix a bug where incorrect conv padding was causing the wrong shaped tensors to be returned.

--- recognition/improved-unet-46986791/modules.py
+++ recognition/improved-unet-46986791/modules.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
         
 class ContextModule(nn.Module):
@@ -64,7 +65,7 @@ class UpsamplingModule(nn.Module):
         super().__init__()
         
         self.upsample = nn.Upsample(scale_factor=2)
-        self.conv = nn.Conv2d(in_channels, in_channels // 2, kernel_size=3)
+        self.conv = nn.Conv2d(in_channels, in_channels // 2, kernel_size=3, padding=1)
         self.act = nn.LeakyReLU(0.01, inplace=True)
 
     def forward(self, x):
@@ -83,11 +84,11 @@ class LocalisationModule(nn.Module):
     def __init__(self, in_channels):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size=3)
+        self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1)
         self.act1 = nn.LeakyReLU(0.01, inplace=True)
 
         self.conv2 = nn.Conv2d(in_channels, in_channels // 2, kernel_size=1)
-        self.act1 = nn.LeakyReLU(0.01, inplace=True)
+        self.act2 = nn.LeakyReLU(0.01, inplace=True)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -116,7 +117,7 @@ class SegmentationLayer(nn.Module):
 class ImprovedUNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.classes = 6
+        self.class_count = 6
 
         filter_sizes = [16, 32, 64, 128, 256]
         
@@ -137,7 +138,32 @@ class ImprovedUNet(nn.Module):
         self.context_3 = ContextModule(filter_sizes[3])
 
         self.downsample_4 = DownsamplingModule(filter_sizes[3], filter_sizes[4])
+        
+        # Bottleneck
         self.context_4 = ContextModule(filter_sizes[4])
+
+        # Decoder
+        self.upsample_4 = UpsamplingModule(filter_sizes[4])
+
+        self.localisation_3 = LocalisationModule(filter_sizes[3] * 2)
+        self.upsample_3 = UpsamplingModule(filter_sizes[3])
+
+        self.localisation_2 = LocalisationModule(filter_sizes[2] * 2)
+        self.seg_2 = SegmentationLayer(filter_sizes[2], self.class_count)
+        self.upsample_2 = UpsamplingModule(filter_sizes[2])
+
+        self.localisation_1 = LocalisationModule(filter_sizes[1] * 2)
+        self.seg_1 = SegmentationLayer(filter_sizes[1], self.class_count)
+        self.upsample_1 = UpsamplingModule(filter_sizes[1])
+
+        self.final_conv = nn.Conv2d(filter_sizes[0] * 2, filter_sizes[1], kernel_size=3, padding=1)
+        self.seg_0 = SegmentationLayer(filter_sizes[1], self.class_count)
+
+        # Deep supervision upscaling
+        self.upscale_seg_2 = nn.Upsample(scale_factor=4, mode='bilinear')
+        self.upscale_seg_1 = nn.Upsample(scale_factor=2, mode='bilinear')
+
+        self.output = nn.Softmax(dim=1)
 
     def forward(self, x):
 
@@ -157,4 +183,35 @@ class ImprovedUNet(nn.Module):
         x = self.downsample_4(skip3)
         x = self.context_4(x)
 
-        return x
+        # Decoder
+        x = self.upsample_4(x)
+        
+        x = torch.cat([x, skip3], dim=1)
+
+        x = self.localisation_3(x)
+        x = self.upsample_3(x)
+
+        x = torch.cat([x, skip2], dim=1)
+        x = self.localisation_2(x)
+        seg_lvl_2 = self.seg_2(x)
+        x = self.upsample_2(x)
+
+        x = torch.cat([x, skip1], dim=1)
+        x = self.localisation_1(x)
+        seg_lvl_1 = self.seg_1(x)
+        x = self.upsample_1(x)
+
+        x = torch.cat([x, skip0], dim=1)
+        x = self.final_conv(x)
+        seg_lvl_0 = self.seg_0(x)
+
+        # deep supervision
+        upscale_lvl_2 = self.upscale_seg_2(seg_lvl_2)
+        upscale_lvl_1 = self.upscale_seg_1(seg_lvl_1)
+
+        out = upscale_lvl_2 + upscale_lvl_1
+        out = out + seg_lvl_0
+
+        out = self.output(out)
+        
+        return out
