8ec6f2611e83dda3fa87395e526e139f1d9aec77
Thomas Day <git@tday.au>
2025-10-28 08:49:55 +1000

    Implement dice loss function from the improved unet paper

---
+++ recognition/improved-unet-46986791/utils.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, u, v):
+        # From the paper:
+        # - "u is the softmax output of the network"
+        # - "v is a one hot encoding of the ground truth segmentation map"
+        # the equation is basically summed intersection over union multiplied by -2/(num classes)
+
+        # flatten
+        u_flat = u.flatten(start_dim=2)
+        v_flat = v.flatten(start_dim=2)
+        
+        # Numerator sigmas
+        intersection = (u_flat * v_flat).sum(dim=2)
+        union = u_flat.sum(dim=2) + v_flat.sum(dim=2)
+
+        # Evaluate fraction
+        dice_per_class = intersection / union
+        
+        # Big sigma in front of fraction
+        dice_sum = dice_per_class.sum(dim=1)
+
+        # Modifier out front of loss equation (6 classes, -2 constant from paper)
+        loss = (-2)/6 * dice_sum
+
+        return loss
