662c68a4d59714a690f8e1f62b38cf79cca71def
Thomas Day <git@tday.au>
2025-10-27 16:53:51 +1000

    Implement context module from the improved UNet paper.

---
+++ recognition/improved-unet-46986791/modules.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+        
+class ContextModule(nn.Module):
+    """
+    implementation of the context module from the improved unet paper (which is a modified residual layer from the Identity Mappings in Deep Residual Networks paper)
+    TODO: cite
+    """
+    def __init__(self, in_channels):
+        super().__init__()
+
+        # Batchnorm replaced by instance norm in the paper for small batch sizes
+        # TODO: since we will have larger batch sizes (we training in 2d) it may make sense to switch back to batchnorm??
+        self.norm1 = nn.InstanceNorm2d(in_channels)
+        self.act1 = nn.LeakyReLU(0.01, inplace=True)
+        self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size=3)
+
+        self.dropout = nn.Dropout2d(p=0.3)
+
+        self.norm2 = nn.InstanceNorm2d(in_channels)
+        self.act2 = nn.LeakyReLU(0.01, inplace=True)
+        self.conv2 = nn.Conv2d(in_channels, in_channels, kernel_size=3)
+
+    def forward(self, x):
+        residual = x
+        x = self.norm1(x)
+        x = self.act1(x)
+        x = self.conv1(x)
+
+        x = self.dropout(x)
+        
+        x = self.norm2(x)
+        x = self.act2(x)
+        x = self.conv2(x)
+
+        x = x + residual
+        
+        return x
