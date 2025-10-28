ebd434a96a3c057475f63d228893da4d52104d7d
Thomas Day <git@tday.au>
2025-10-28 13:54:58 +1000

    Validate model each epoch during training

--- recognition/improved-unet-46986791/train.py
+++ recognition/improved-unet-46986791/train.py
@@ -11,19 +11,24 @@ device = 'mps'
 train_ds = HipMRIDataset('./data/keras_slices_data/keras_slices_train', './data/keras_slices_data/keras_slices_seg_train')
 train_loader = DataLoader(train_ds, batch_size=16, shuffle=True)
 
+val_ds = HipMRIDataset('./data/keras_slices_data/keras_slices_validate', './data/keras_slices_data/keras_slices_seg_validate')
+val_loader = DataLoader(val_ds, batch_size=16, shuffle=True)
+
 model = ImprovedUNet().to(device)
 loss_fn = DiceLoss()
 optimiser = torch.optim.Adam(model.parameters(), lr=1e-3)
 
 num_epochs = 10
 for epoch in range(num_epochs):
+    print(f"======== Epoch {epoch}/{num_epochs} ======")
+
     train_loss = 0.0
     loss_history = []
     dice_class_scores = []
     
     loading_bar = tqdm(train_loader)
     model.train()
-    print(f"Training epoch {epoch}/{num_epochs}")
+    print(f"Training...")
     for images, segs in loading_bar:
         images = images.to(device)
         segs = segs.to(device)
@@ -46,4 +51,31 @@ for epoch in range(num_epochs):
     dice_class_scores = torch.stack(dice_class_scores)
     mean_dice_class_scores = torch.nanmean(dice_class_scores, dim=0)
     
-    print(f"class-by-class mean dice score: {mean_dice_class_scores}")
+    print(f"class-by-class mean dice score on train set: {mean_dice_class_scores}")
+
+
+    # Validation
+    loading_bar = tqdm(val_loader)
+    model.eval()
+
+    val_loss = 0
+    loss_history = []
+    dice_class_scores = []
+    
+    print(f"Validating...")
+    for images, segs in loading_bar:
+        images = images.to(device)
+        segs = segs.to(device)
+
+        outputs = model(images)
+
+        val_loss += loss.item()
+        loss_history.append(loss.item())
+
+        dice_class_scores.append(per_class_score_dice(outputs, segs))
+        loading_bar.set_postfix({"Total loss": f"{train_loss:.4f}", "mean loss": f"{(train_loss/len(loss_history)):.4f}"})
+    
+    dice_class_scores = torch.stack(dice_class_scores)
+    mean_dice_class_scores = torch.nanmean(dice_class_scores, dim=0)
+    
+    print(f"class-by-class mean dice score on validation set: {mean_dice_class_scores}")
