bfecec2ddb2368e4f1fcc79be25a25d668f1f7e2
Thomas Day <git@tday.au>
2025-10-29 11:24:09 +1000

    Add training statistic logging to CSV. Fix bug where incorrect loss was being calculated for validation. Fix bug where printing in the wrong spot was causing two loading bars to be printed. Stop gradients on validation set.

--- recognition/improved-unet-46986791/train.py
+++ recognition/improved-unet-46986791/train.py
@@ -5,6 +5,7 @@ from modules import ImprovedUNet
 from utils import DiceLoss, per_class_score_dice
 from tqdm.auto import tqdm
 import os
+import pandas as pd
 
 device = 'mps'
 checkpoint_dir = './checkpoints'
@@ -21,18 +22,26 @@ model = ImprovedUNet().to(device)
 loss_fn = DiceLoss()
 optimiser = torch.optim.Adam(model.parameters(), lr=1e-3)
 
+# Logging
+train_csv_dir = f'./{checkpoint_dir}/train_metrics.csv'
+train_metrics = []
+val_csv_dir = f'./{checkpoint_dir}/val_metrics.csv'
+val_metrics = []
+
+
 num_epochs = 10
 for epoch in range(num_epochs):
     print(f"======== Epoch {epoch}/{num_epochs} ======")
+
+    print(f"Training...")
+    loading_bar = tqdm(train_loader)
 
     train_loss = 0.0
     loss_history = []
     dice_class_scores = []
     
-    loading_bar = tqdm(train_loader)
     model.train()
-    print(f"Training...")
-    for images, segs in loading_bar:
+    for batch_index, (images, segs) in enumerate(loading_bar):
         images = images.to(device)
         segs = segs.to(device)
 
@@ -47,9 +56,26 @@ for epoch in range(num_epochs):
         train_loss += loss.item()
         loss_history.append(loss.item())
 
-        dice_class_scores.append(per_class_score_dice(outputs, segs))
+        class_dice = per_class_score_dice(outputs, segs)
+        dice_class_scores.append(class_dice)
 
         loading_bar.set_postfix({"Total loss": f"{train_loss:.4f}", "mean loss": f"{(train_loss/len(loss_history)):.4f}"})
+
+        # log batch
+        train_metrics.append({
+            'epoch': epoch,
+            'batch': batch_index,
+            'batch_loss': loss.item(),
+            'class_0_dice': class_dice[0].item(),
+            'class_1_dice': class_dice[1].item(),
+            'class_2_dice': class_dice[2].item(),
+            'class_3_dice': class_dice[3].item(),
+            'class_4_dice': class_dice[4].item(),
+            'class_5_dice': class_dice[5].item(),
+            'batch_mean_dice': class_dice.mean().item(),
+        })
+
+    pd.DataFrame(train_metrics).to_csv(train_csv_dir)
 
     dice_class_scores = torch.stack(dice_class_scores)
     mean_dice_class_scores = torch.nanmean(dice_class_scores, dim=0)
@@ -58,6 +84,7 @@ for epoch in range(num_epochs):
 
 
     # Validation
+    print(f"Validating...")
     loading_bar = tqdm(val_loader)
     model.eval()
 
@@ -65,22 +92,40 @@ for epoch in range(num_epochs):
     loss_history = []
     dice_class_scores = []
     
-    print(f"Validating...")
-    for images, segs in loading_bar:
-        images = images.to(device)
-        segs = segs.to(device)
+    with torch.no_grad():
+        for images, segs in loading_bar:
+            images = images.to(device)
+            segs = segs.to(device)
 
-        outputs = model(images)
+            outputs = model(images)
 
-        val_loss += loss.item()
-        loss_history.append(loss.item())
+            loss = loss_fn(outputs, segs)
+            val_loss += loss.item()
+            loss_history.append(loss.item())
 
-        dice_class_scores.append(per_class_score_dice(outputs, segs))
-        loading_bar.set_postfix({"Total loss": f"{train_loss:.4f}", "mean loss": f"{(train_loss/len(loss_history)):.4f}"})
-    
+            dice_class_scores.append(per_class_score_dice(outputs, segs))
+            loading_bar.set_postfix({"Total loss": f"{val_loss:.4f}", "mean loss": f"{(val_loss/len(loss_history)):.4f}"})
+
+
+
     dice_class_scores = torch.stack(dice_class_scores)
     mean_dice_class_scores = torch.nanmean(dice_class_scores, dim=0)
     
     print(f"class-by-class mean dice score on validation set: {mean_dice_class_scores}")
+
+    # log val epoch
+    val_metrics.append({
+        'epoch': epoch,
+        'mean_loss': (val_loss/len(loss_history)),
+        'class_0_dice': mean_dice_class_scores[0].item(),
+        'class_1_dice': mean_dice_class_scores[1].item(),
+        'class_2_dice': mean_dice_class_scores[2].item(),
+        'class_3_dice': mean_dice_class_scores[3].item(),
+        'class_4_dice': mean_dice_class_scores[4].item(),
+        'class_5_dice': mean_dice_class_scores[5].item(),
+        'epoch_mean_dice': mean_dice_class_scores.mean().item(),
+    })
+
+    pd.DataFrame(val_metrics).to_csv(val_csv_dir)
 
     torch.save(model.state_dict(), f'{checkpoint_dir}/epoch_{epoch}.pth')
