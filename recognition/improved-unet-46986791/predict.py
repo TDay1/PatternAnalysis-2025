905b8b37bc899b99c7fdd47c4c523200d5e05bdd
Thomas Day <git@tday.au>
2025-10-30 15:27:27 +1000

    Add documenting comments to the prediction script. Generally cleanup code.

--- recognition/improved-unet-46986791/predict.py
+++ recognition/improved-unet-46986791/predict.py
@@ -6,34 +6,36 @@ import matplotlib.pyplot as plt
 import argparse
 import os
 
+# Parse CLI args
 parser = argparse.ArgumentParser()
-
 parser.add_argument("-m", "--model-dir", required=True, type=str)
 parser.add_argument("-o", "--output-dir", default="./predict", type=str)
 parser.add_argument("-c", "--count", default=2, type=int)
 args = parser.parse_args()
 os.makedirs(args.output_dir, exist_ok=True)
 
+# For such a low number of examples, using the CPU is fine and simplifies things
 device = "cpu"
 
-model = ImprovedUNet().to(device)
+# Insanciate model + load weights
+model = ImprovedUNet()
 state_dict = torch.load(args.model_dir, map_location=torch.device(device))
 model.load_state_dict(state_dict)
 model.eval()
 
+# Load dataset
 test_ds = HipMRIDataset('./data/keras_slices_data/keras_slices_test', './data/keras_slices_data/keras_slices_seg_test')
 test_loader = DataLoader(test_ds, batch_size=args.count, shuffle=True)
-
-
 images, segs = next(iter(test_loader))
-images = images.to(device)
-segs = segs.to(device)
 
+# Predict
 outputs = model(images)
+
+# Convert the predicitons and labels to label-encoding
 predictions = torch.argmax(outputs, dim=1)
 segs_label = torch.argmax(segs, dim=1)
 
-# plot
+# plot raw segmentations next to input image
 fig, axes = plt.subplots(args.count, 3)
 for i in range(args.count):
     axes[i, 0].imshow(images[i, 0], cmap='gray')
@@ -51,6 +53,7 @@ for i in range(args.count):
 fig.tight_layout()
 fig.savefig(f"{args.output_dir}/segmentation_pred.png")
 
+# Overlay segmentations on input image
 fig, axes = plt.subplots(args.count, 2)
 for i in range(args.count):
     axes[i, 0].imshow(images[i, 0], cmap='gray')
