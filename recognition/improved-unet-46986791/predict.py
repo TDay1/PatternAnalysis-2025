dda4fbc870db701e7ab49287225ffae782278d29
Thomas Day <git@tday.au>
2025-10-30 12:13:09 +1000

    Add the functionality to plot an overlay over the input image in the prediction script.

--- recognition/improved-unet-46986791/predict.py
+++ recognition/improved-unet-46986791/predict.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 
 device = "cpu"
+examples = 2
 
 model = ImprovedUNet().to(device)
 state_dict = torch.load('./checkpoint/epoch_35.pth', map_location=torch.device(device))
@@ -12,8 +13,7 @@ model.load_state_dict(state_dict)
 model.eval()
 
 test_ds = HipMRIDataset('./data/keras_slices_data/keras_slices_test', './data/keras_slices_data/keras_slices_seg_test')
-test_loader = DataLoader(test_ds, batch_size=4, shuffle=True)
-
+test_loader = DataLoader(test_ds, batch_size=examples, shuffle=True)
 
 
 images, segs = next(iter(test_loader))
@@ -28,18 +28,32 @@ print(images.shape)
 print(predictions.shape)
 
 # plot
-fig, axes = plt.subplots(4, 3)
-for i in range(4):
+fig, axes = plt.subplots(examples, 3)
+for i in range(examples):
     axes[i, 0].imshow(images[i, 0], cmap='gray')
     axes[i, 0].set_title("Input MRI")
     axes[i, 0].axis('off')
 
-    axes[i, 1].imshow(segs_label[i])
+    axes[i, 1].imshow(segs_label[i], vmin=0, vmax=5)
     axes[i, 1].set_title("Ground Truth Segmentation")
     axes[i, 1].axis('off')
 
     axes[i, 2].imshow(predictions[i])
     axes[i, 2].set_title("Predicted Segmentation")
+    axes[i, 2].axis('off')
+
+fig, axes = plt.subplots(examples, 2)
+for i in range(examples):
+    axes[i, 0].imshow(images[i, 0], cmap='gray')
+    axes[i, 0].imshow(segs_label[i], alpha=0.5, cmap="tab10", vmin=0, vmax=5)
+    axes[i, 0].set_title("Ground Truth")
+    axes[i, 0].axis('off')
+
+    axes[i, 1].imshow(images[i, 0], cmap='gray')
+    axes[i, 1].imshow(predictions[i], alpha=0.5, cmap="tab10", vmin=0, vmax=5)
+
+    axes[i, 1].set_title("Predicted")
     axes[i, 1].axis('off')
 
+plt.tight_layout()
 plt.show()
